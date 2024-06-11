e6275414418fb7b595f1e64eb8e6884912355bde
Derya <derya.tasdemir.00@gmail.com>
2024-06-11 09:22:54 +0300

    Custom ScroolView changed

--- components/ParallaxScrollView.tsx
+++ components/ParallaxScrollView.tsx
@@ -1,17 +1,14 @@
-import type { PropsWithChildren, ReactElement } from 'react';
+import type { ReactNode } from 'react';
 import { StyleSheet } from 'react-native';
 import Animated, { useAnimatedRef} from 'react-native-reanimated';
 
 import { ThemedView } from '@/components/ThemedView';
 
-type Props = PropsWithChildren<{
-  headerImage: ReactElement;
-  headerBackgroundColor: { dark: string; light: string };
-}>;
+interface Props {
+  children: ReactNode;}
 
-export default function ParallaxScrollView({
-  children,
-}: Props) {
+export default function ParallaxScrollView({ children } : Props) {
+  
   const scrollRef = useAnimatedRef<Animated.ScrollView>();
 
   return (
